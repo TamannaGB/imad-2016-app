ba70c513e2b558e58f6ac1bd71533858c44503ea
TamannaGB <tamanna.cs.du.2013@gmail.com>
2016-10-07 14:20:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
-console.log('Loaded!');
+var submit=document.getElementById("submit_comment");
+submit.onclick = function(){
+    var request = new XMLhttpRequest();
+    request.onreadyatchange = function(){
+        if(request.readyState == XMLhttpRequest.DONE){
+            if(request.status == 200){
+                var commecnts=request.responseText;
+                comments=JSON.parse(comments);
+                var list='';
+                for(var i=0;i<comments.length;i++){
+                    list = '<li>'+comments
+                }
+                
+            }
+            
+        }
+    }
+}
