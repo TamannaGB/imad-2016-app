d84a43d268a92c9d9734753fc17ff686f95a8afb
TamannaGB <tamanna.cs.du.2013@gmail.com>
2016-10-07 14:03:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,9 +101,9 @@ var htmlTemplate=`<html>
         <hr>
         <h4> Add a Comment </h4>
         <div> 
-        <input type="text" name="Add a Comment"><br>
-        <input type="text" name="User Id"><br>
-        <input type="submit" value="Submit">
+        <input type="text" name="Add a Comment"/><br>
+        <input type="text" name="User Id"/><br>
+        <input type="submit" value="Submit"/>
         </div>
     </div>    
     </body>
