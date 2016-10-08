a301976bd892e698a446cd56b005cde82bef6f42
TamannaGB <tamanna.cs.du.2013@gmail.com>
2016-10-08 15:14:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 var button=document.getElementById("submit_comment");
 button.onclick = function(){
 
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
+    var comment = document.getElementById("comment");
+    var data = comment.value;
+    var user = document.getElementById("user_id");
+    var name = user.value;
+    if(data !== null && name !== null ){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status == 200){
                 var comments=request.responseText;
@@ -16,11 +21,9 @@ button.onclick = function(){
             }
             
         }
-    };
-    var comment = document.getElementById("comment");
-    var data = comment.value;
-    var user = document.getElementById("user_id");
-    var name = user.value;
-    request.open('GET','http://tamannagb.imad.hasura-app.io/submit-comment?comment='+data+'&user_id='+name,true);
-    request.send(null);
+        };
+        
+        request.open('GET','http://tamannagb.imad.hasura-app.io/submit-comment?comment='+data+'&user_id='+name,true);
+        request.send(null);
+    }
 };
