519a5de6cebf92e59a501f0862e8081bfb27abba
TamannaGB <tamanna.cs.du.2013@gmail.com>
2016-10-08 14:43:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/', function (req, res) {
 
 var comments=[];
 app.get('/submit-comment',function(req,res){
-   var comment=req.query.name;
+   var comment=req.query.comment;
    comments.push(comment);
    res.send(JSON.stringify(comments));
 });
